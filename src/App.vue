13cf7cc2047ed71ed8d9f651036fd4f9c5cd76f2
jenolah <olahjendev@gmail.com>
2025-04-24 17:21:42 +0200

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Simulator from './components/Simulator.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img src="/plus.svg" alt="Logo" />
+    <h1 class="title">Lottery Simulator</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <Simulator />
   </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '@/assets/base';
+
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  display: flex;
+  gap: 1rem;
+  color: white;
+  background: linear-gradient(90deg, base.$primary-color 0%, base.$secondary-color 100%);
+  padding: 1rem;
+  margin-bottom: 1rem;
 }
 </style>
