ebcbeefcd3b60897e0a6eb2badfa984ea6cd1eb8
jenolah <olahjendev@gmail.com>
2025-04-24 17:22:01 +0200

    create metrics

---
+++ src/components/Metrics.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { numberOfTickets } = defineProps(['numberOfTickets'])
+</script>
+
+<template>
+  <div class="metrics grid">
+    <div>Number of tickets:</div>
+    <div>{{ numberOfTickets }}</div>
+    <div>Years spent:</div>
+    <div>{{ Math.round(numberOfTickets / 52) }}</div>
+    <div>Cost of tickets:</div>
+    <div>{{ numberOfTickets * 300 }} HUF</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/base';
+</style>
