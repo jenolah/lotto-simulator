00f0b25340fe5a62327921e7aeb50f9325888baa
jenolah <olahjendev@gmail.com>
2025-04-24 18:10:26 +0200

    format metrics numbers

--- src/components/Metrics.vue
+++ src/components/Metrics.vue
@@ -1,15 +1,20 @@
 <script setup>
 const { numberOfTickets } = defineProps(['numberOfTickets'])
+const formatNumber = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+}
 </script>
 
 <template>
   <div class="metrics grid">
     <div>Number of tickets:</div>
     <div>{{ numberOfTickets }}</div>
+    <div class="bold">Number of tickets:</div>
+    <div class="bold">{{ formatNumber(numberOfTickets) }}</div>
     <div>Years spent:</div>
-    <div>{{ Math.round(numberOfTickets / 52) }}</div>
+    <div>{{ formatNumber(Math.round(numberOfTickets / 52)) }}</div>
     <div>Cost of tickets:</div>
-    <div>{{ numberOfTickets * 300 }} HUF</div>
+    <div>{{ formatNumber(numberOfTickets * 300) }} HUF</div>
   </div>
 </template>
 
